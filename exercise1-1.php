c73db899cfd942a6b73f2aac3889596a18ec2a77
NoaDreams <syuhadazamzuri2002@gmail.com>
2025-07-14 04:11:45 +0800

    Add files via upload

---
+++ exercise1-1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercise 1.1: Basic Function</title>
+</head>
+<body>
+    <h1>Exercise 1.1: Basic Function</h1>
+    <?php
+    function writeMsg() {
+        echo "Hello world!";
+    }
+    writeMsg(); // Call the function
+    ?>
+</body>
+</html>
